9e2bd479f6e98ec4049383b28aeae7ea448a65e1
zsk13 <1641711860@qq.com>
2017-12-08 11:46:59 +0800

    user

--- src/main/java/network/model/Question.java
+++ src/main/java/network/model/Question.java
@@ -6,6 +6,36 @@ public class Question {
     private String question;
     private String answer;
     private String status;
+    public Long getQid() {
+        return qid;
+    }
+    public void setQid(Long qid) {
+        this.qid = qid;
+    }
+    public Long getTeacher_id() {
+        return teacher_id;
+    }
+    public void setTeacher_id(Long teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
     
 }
