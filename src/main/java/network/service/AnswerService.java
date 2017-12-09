094d9c037b128f58099f08a900b036e98d5e2c5b
xuhe <mf1732148@smail.nju.edu.cn>
2017-12-09 16:29:58 +0800

    add answerServiceImpl

--- src/main/java/network/service/AnswerService.java
+++ src/main/java/network/service/AnswerService.java
@@ -1,4 +1,7 @@
 package network.service;
 
+import java.util.List;
+
 public interface AnswerService {
+    public List<Integer> getAnswerNum(Long qid);
 }
--- src/main/java/network/service/impl/AnswerServiceImpl.java
+++ src/main/java/network/service/impl/AnswerServiceImpl.java
@@ -1,4 +1,39 @@
 package network.service.impl;
 
-public class AnswerServiceImpl {
+import network.dao.AnswerMapper;
+import network.model.Answer;
+import network.model.AnswerExample;
+import network.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AnswerServiceImpl implements AnswerService{
+    @Autowired
+    AnswerMapper answerMapper;
+    public List<Answer> getAnswer(Long qid){
+        AnswerExample answerExample = new AnswerExample();
+        AnswerExample.Criteria criteria = answerExample.createCriteria();
+        criteria.andQidEqualTo(qid);
+        return  answerMapper.selectByExample(answerExample);
+    }
+    public List<Integer> getAnswerNum(Long qid){
+        List<Integer> num = null;
+        List<Answer> listAnswer = getAnswer(qid);
+        int total = listAnswer.size();
+        num.add(total);
+        int correctNum = 0;
+        boolean answer;
+        for (Answer t : listAnswer) {
+            answer = t.getCorrect();
+            if(answer){
+                correctNum++;
+            }
+        }
+        num.add(correctNum);
+        int wrongNum = total - correctNum;
+        num.add(wrongNum);
+        return  num;
+    }
+    
 }
