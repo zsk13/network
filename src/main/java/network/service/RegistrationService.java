595862f542e5f24ce918bf0b4b0fe67a00dcd138
zhuhuihui <1360245308@qq.com>
2017-12-11 20:17:53 +0800

    新增添加签到功能

--- src/main/java/network/service/RegistrationService.java
+++ src/main/java/network/service/RegistrationService.java
@@ -1,7 +1,10 @@
 package network.service;
 
+import network.model.Registration;
+
 import java.util.Date;
 
 public interface RegistrationService {
     public int registration(double location_x, double location_y, String openId, Date time);
+    public void add(Registration registration);
 }
