59223df62376cfe6ed65ef0a680e182dcc788df5
zsk13 <1641711860@qq.com>
2017-12-11 20:29:49 +0800

    questionlist

--- src/main/java/network/controller/WeChatCtr.java
+++ src/main/java/network/controller/WeChatCtr.java
@@ -46,6 +46,7 @@ public class WeChatCtr {
             String timestamp = request.getParameter("timestamp");// 时间戳  
             String nonce = request.getParameter("nonce");// 随机数  
             String echostr = request.getParameter("echostr");// 随机字符串  
+            response.getWriter().print(echostr);  
             PrintWriter out = response.getWriter();  
             // 将token、timestamp、nonce三个参数进行字典序排序  
             String[] params = new String[] { TOKEN, timestamp, nonce };  
@@ -57,11 +58,15 @@ public class WeChatCtr {
                     Hex.encodeHex(MessageDigest.getInstance(algorithm).digest((clearText).getBytes()), true));  
             // 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信  
             if (signature.equals(sign)) {  
-                response.getWriter().print(echostr);  
-            }  
+                //response.getWriter().print(echostr);  
+            }else{  
+                //response.getWriter().print(echostr);
+            }
         } catch (Exception e) {  
+            
             e.printStackTrace();  
         }  
+        
     }
     
     @RequestMapping(value = "validate", method = {RequestMethod.POST})
@@ -82,7 +87,7 @@ public class WeChatCtr {
             String content = map.get("Content");
             // 对消息进行处理
             if (WechatMessageUtil.MESSAGE_TEXT.equals(msgType)) {// 文本消息
-                if (content.startsWith("学号：")) {
+                if (content.startsWith("学号")) {
                     followService.follow(map,out,content);
                     return;
                 }
