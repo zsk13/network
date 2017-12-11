bb18219fc3b43cfaed0814e4e998029733e3f2a2
zsk13 <1641711860@qq.com>
2017-12-11 20:48:40 +0800

    questionlist

--- src/main/java/network/service/impl/FollowServiceImpl.java
+++ src/main/java/network/service/impl/FollowServiceImpl.java
@@ -1,5 +1,7 @@
 package network.service.impl;
 
+import network.common.wechatUtil.TextMessage;
+import network.common.wechatUtil.WechatMessageUtil;
 import network.dao.UsersDao;
 import network.model.Users;
 import network.service.FollowService;
@@ -11,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FollowServiceImpl implements FollowService{
+public class FollowServiceImpl implements FollowService {
     @Autowired
     private UsersDao usersDao;
 
@@ -27,15 +29,31 @@ public class FollowServiceImpl implements FollowService{
 
     @Override
     public void follow(Map<String, String> map, PrintWriter out, String content) {
-        String[] ss  = content.split(" ");
         String fromUserName = map.get("FromUserName");
-        int index = content.indexOf(",");
-        String sno = ss[1];
-        String name = ss[3];
-        Users users = new Users();
-        users.setuOpenId(fromUserName);
-        users.setSno(sno);
-        users.setName(name);
-        usersDao.insert(users);
+        String toUserName = map.get("ToUserName");
+        TextMessage textMessage = new TextMessage();
+        textMessage.setMsgType(WechatMessageUtil.MESSAGE_TEXT);
+        textMessage.setToUserName(fromUserName);
+        textMessage.setFromUserName(toUserName);
+        textMessage.setCreateTime(System.currentTimeMillis());
+
+
+        try {
+            String[] ss = content.split(" ");
+            String sno = ss[1];
+            String name = ss[3];
+            Users users = new Users();
+            users.setuOpenId(fromUserName);
+            users.setSno(sno);
+            users.setName(name);
+            usersDao.insert(users);
+            textMessage.setContent("注册成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            textMessage.setContent("格式应该为 学号 MF1732222 姓名 张三");
+        }
+        String responseMessage = WechatMessageUtil.textMessageToXml(textMessage);
+        out.print(responseMessage);
+        out.flush();
     }
 }
