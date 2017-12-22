b68ab3a88c4b35b33db5c1cde8e3128a2b1c0112
zhuyuanfu <641561609@qq.com>
2017-12-22 18:43:20 +0800

    re-design a bean

--- src/main/java/network/model/Registration.java
+++ src/main/java/network/model/Registration.java
@@ -3,63 +3,47 @@ package network.model;
 import java.util.Date;
 
 public class Registration {
-    private Long rId;
-
-    private String name;
-
-    private Long lId;
-
-    private Date sTime;
-
-    private Date eTime;
-
-    private String className;
-
-    public Long getrId() {
-        return rId;
-    }
-
-    public void setrId(Long rId) {
-        this.rId = rId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public Long getlId() {
-        return lId;
-    }
-
-    public void setlId(Long lId) {
-        this.lId = lId;
-    }
-
-    public Date getsTime() {
-        return sTime;
-    }
-
-    public void setsTime(Date sTime) {
-        this.sTime = sTime;
-    }
-
-    public Date geteTime() {
-        return eTime;
-    }
-
-    public void seteTime(Date eTime) {
-        this.eTime = eTime;
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
-        this.className = className == null ? null : className.trim();
-    }
+	private long rId;
+	private long lId;
+	private Date sTime;
+	private Date eTime;
+	private long cId;
+	private String cName;
+	public long getrId() {
+		return rId;
+	}
+	public void setrId(long rId) {
+		this.rId = rId;
+	}
+	public long getlId() {
+		return lId;
+	}
+	public void setlId(long lId) {
+		this.lId = lId;
+	}
+	public Date getsTime() {
+		return sTime;
+	}
+	public void setsTime(Date sTime) {
+		this.sTime = sTime;
+	}
+	public Date geteTime() {
+		return eTime;
+	}
+	public void seteTime(Date eTime) {
+		this.eTime = eTime;
+	}
+	public long getcId() {
+		return cId;
+	}
+	public void setcId(long cId) {
+		this.cId = cId;
+	}
+	public String getcName() {
+		return cName;
+	}
+	public void setcName(String cName) {
+		this.cName = cName;
+	}
+	
 }
