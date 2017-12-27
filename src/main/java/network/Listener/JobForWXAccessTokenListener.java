135c46554b2292ecdfc2ed7ba5c27e8abcfa844e
zsk13 <1641711860@qq.com>
2017-12-27 16:59:18 +0800

    233

--- src/main/java/network/Listener/JobForWXAccessTokenListener.java
+++ src/main/java/network/Listener/JobForWXAccessTokenListener.java
@@ -8,7 +8,7 @@ import network.common.AccessTokenUtil;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-@Component("jobForWXAccessTokenListener1")
+@Component("jobForWXAccessTokenListener2")
 public class JobForWXAccessTokenListener implements ApplicationListener<ContextRefreshedEvent> {
 
     public void onApplicationEvent(ContextRefreshedEvent event) {
